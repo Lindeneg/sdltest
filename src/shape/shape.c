fb3355bf283c584ac58aad347746c2fe087898d5
Christian <christianlindeneg@gmail.com>
2020-04-19 17:05:07 +0200

    adding new shapes

--- src/shape/shape.c
+++ src/shape/shape.c
@@ -1,52 +1,156 @@
 #include <stddef.h>
+#include <stdio.h>
 
 #include "../shape/shape.h"
 #include "../util/constants.h"
 #include "../util/util.h"
 
 
+static int mRemoveLine(ShapeArray *shapes, void *shape) {
+    size_t targetIndex = GetShapeIndex(shapes, shape, LINE);
+    if (targetIndex >= 0) {
+        if (shapes->lineArray->size == 1) {
+            FreeArray(shapes->lineArray->lines, shapes->lineArray->size, LINE);
+            shapes->lineArray->size = 0;
+        } else {
+            FreeComponent(shapes->lineArray->lines[targetIndex], LINE);
+            for (size_t i = targetIndex + 1; i < shapes->lineArray->size; i++) {
+                shapes->lineArray->lines[i-1] = shapes->lineArray->lines[i];
+            }
+            if (shapes->lineArray->size > 1) {
+                ReAllocMem(shapes->lineArray->lines, (shapes->lineArray->size - 1) * sizeof(Line*));
+            }
+            shapes->lineArray->size--;
+        }
+        return 0;
+    }
+    printf("\nRemoveLineError: could not remove %p with index %d", shape, targetIndex);
+    return -1;
+}
+
+static void mAddLine(ShapeArray *shapes, void *shape) {
+    Line *line = (Line*)shape;
+    if (!(shapes->lineArray)) {
+        shapes->lineArray = (LineArray*)AllocMem(sizeof(LineArray));
+        shapes->lineArray->lines = (Line**)AllocMem(sizeof(Line*));
+        shapes->lineArray->lines[0] = line;
+        shapes->lineArray->size = 1;
+    } else {
+        ReAllocMem(shapes->lineArray->lines, (shapes->lineArray->size + 1) * sizeof(Line*));
+        shapes->lineArray->size++;
+        shapes->lineArray->lines[(shapes->lineArray->size - 1)] = line;
+    }
+}
+
+static int mRemoveTriangle(ShapeArray *shapes, void *shape) {
+    size_t targetIndex = GetShapeIndex(shapes, shape, TRIANGLE);
+    if (targetIndex >= 0) {
+        if (shapes->triangleArray->size == 1) {
+            FreeArray(shapes->triangleArray->triangles, shapes->triangleArray->size, TRIANGLE);
+            shapes->triangleArray->size = 0;
+        } else {
+            FreeComponent(shapes->triangleArray->triangles[targetIndex], TRIANGLE);
+            for (size_t i = targetIndex + 1; i < shapes->triangleArray->size; i++) {
+                shapes->triangleArray->triangles[i-1] = shapes->triangleArray->triangles[i];
+            }
+            if (shapes->triangleArray->size > 1) {
+                ReAllocMem(shapes->triangleArray->triangles, (shapes->triangleArray->size - 1) * sizeof(Triangle*));
+            }
+            shapes->triangleArray->size--;
+        }
+        return 0;
+    }
+    printf("\nRemoveTriangle: could not remove %p with index %d", shape, targetIndex);
+    return -1;
+}
+
+static void mAddTriangle(ShapeArray *shapes, void *shape) {
+    Triangle *triangle = (Triangle*)shape;
+    if (!(shapes->triangleArray)) {
+        shapes->triangleArray = (TriangleArray*)AllocMem(sizeof(TriangleArray));
+        shapes->triangleArray->triangles = (Triangle**)AllocMem(sizeof(Triangle*));
+        shapes->triangleArray->triangles[0] = triangle;
+        shapes->triangleArray->size = 1;
+    } else {
+        ReAllocMem(shapes->triangleArray->triangles, (shapes->triangleArray->size + 1) * sizeof(Triangle*));
+        shapes->triangleArray->size++;
+        shapes->triangleArray->triangles[(shapes->triangleArray->size - 1)] = triangle;
+    }
+}
+
+static int mRemoveRectangle(ShapeArray *shapes, void *shape) {
+    size_t targetIndex = GetShapeIndex(shapes, shape, RECTANGLE);
+    if (targetIndex >= 0) {
+        if (shapes->rectangleArray->size == 1) {
+            FreeArray(shapes->rectangleArray->rectangles, shapes->rectangleArray->size, RECTANGLE);
+            shapes->rectangleArray->size = 0;
+        } else {
+            FreeComponent(shapes->rectangleArray->rectangles[targetIndex], RECTANGLE);
+            for (size_t i = targetIndex + 1; i < shapes->rectangleArray->size; i++) {
+                shapes->rectangleArray->rectangles[i-1] = shapes->rectangleArray->rectangles[i];
+            }
+            if (shapes->rectangleArray->size > 1) {
+                ReAllocMem(shapes->rectangleArray->rectangles, (shapes->rectangleArray->size - 1) * sizeof(Rectangle*));
+            }
+            shapes->rectangleArray->size--;
+        }
+        return 0;
+    }
+    printf("\nRemoveRectangle: could not remove %p with index %d", shape, targetIndex);
+    return -1;
+}
+
+static void mAddRectangle(ShapeArray *shapes, void *shape) {
+    Rectangle *rectangle = (Rectangle*)shape;
+    if (!(shapes->rectangleArray)) {
+        shapes->rectangleArray = (RectangleArray*)AllocMem(sizeof(RectangleArray));
+        shapes->rectangleArray->rectangles = (Rectangle**)AllocMem(sizeof(Rectangle*));
+        shapes->rectangleArray->rectangles[0] = rectangle;
+        shapes->rectangleArray->size = 1;
+    } else {
+        ReAllocMem(shapes->rectangleArray->rectangles, (shapes->rectangleArray->size + 1) * sizeof(Rectangle*));
+        shapes->rectangleArray->size++;
+        shapes->rectangleArray->rectangles[(shapes->rectangleArray->size - 1)] = rectangle;
+    }
+}
 
 ShapeArray *CreateEmptyShapeArray(void) {
     ShapeArray * shapeArray = (ShapeArray*)AllocMem(sizeof(ShapeArray));
     shapeArray->lineArray = NULL;
+    shapeArray->triangleArray = NULL;
+    shapeArray->rectangleArray = NULL;
     return shapeArray;
 }
-
 
 int AddShapeToArray(ShapeArray *shapes, void *shape, const unsigned int type) {
     if (shapes) {
         if (type == LINE) {
-            Line *line = (Line*)shape;
-            if (!(shapes->lineArray)) {
-                shapes->lineArray = (LineArray*)AllocMem(sizeof(LineArray));
-                shapes->lineArray->lines = (Line**)AllocMem(sizeof(Line*));
-                shapes->lineArray->lines[0] = line;
-                shapes->lineArray->size = 1;
-            } else {
-                ReAllocMem(shapes->lineArray->lines, (shapes->lineArray->size + 1) * sizeof(Line*));
-                shapes->lineArray->size++;
-                shapes->lineArray->lines[(shapes->lineArray->size - 1)] = line;
-            }
-            return 0;
+            mAddLine(shapes, shape);
         }
+        if (type == TRIANGLE) {
+            mAddTriangle(shapes, shape);
+        }
+        if (type == RECTANGLE) {
+            mAddRectangle(shapes, shape);
+        }
+        return 0;
     }
+    printf("\nAddShapeToArrayError: could not add shape %p as ShapeArray %p returns a falsy value", shape, shapes);
     return -1;
 }
 
-// int RemoveShapeFromArray(ShapeArray *shapes, void *shape, const unsigned int type) {
-//     if (shapes) {
-//         if (type == LINE) {
-//             Line *line = (Line*)shape;
-//             for (size_t i = 0; i < shapes->lineArray->size; i++) {
-//                 if (AreEqualLines(shapes->lineArray->lines[i], line)) {
-//                     FreeMem(shapes->lineArray->lines[i]->p0);
-//                     FreeMem(shapes->lineArray->lines[i]->p1);
-//                     FreeMem(shapes->lineArray->lines[i]);
-//                     ReAllocMem(shapes, (shapes->lineArray->size - 1) * sizeof(Line*));
-//                     shapes->lineArray->size--;
-//                     return 0;
-//                 }
-//             }
-//         }
-//     }
-// }
+int RemoveShapeFromArray(ShapeArray *shapes, void *shape, const unsigned int type) {
+    if (shapes) {
+        if ((type == LINE) && (shapes->lineArray) && (shapes->lineArray->size > 0)) {
+            return mRemoveLine(shapes, shape);
+        }
+        if ((type == TRIANGLE) && (shapes->triangleArray) && (shapes->triangleArray->size > 0)) {
+            return mRemoveTriangle(shapes, shape);
+        }
+        if ((type == RECTANGLE) && (shapes->rectangleArray) && (shapes->rectangleArray->size > 0)) {
+            return mRemoveRectangle(shapes, shape);
+        }
+    }
+    printf("\nRemoveShapeFromArrayError: could not remove shape %p as ShapeArray %p returns a falsy value", shape, shapes);
+    return -1;
+}
--- src/shape/shape.h
+++ src/shape/shape.h
@@ -2,6 +2,8 @@
 #define SHAPE_SHAPE_H_
 
 #include "../component/line.h"
+#include "../component/triangle.h"
+#include "../component/rectangle.h"
 
 
 struct s_line_array {
@@ -10,9 +12,22 @@ struct s_line_array {
 };
 typedef struct s_line_array LineArray;
 
+struct s_triangle_array {
+    Triangle **triangles;
+    size_t size;
+};
+typedef struct s_triangle_array TriangleArray;
+
+struct s_rectangle_array {
+    Rectangle **rectangles;
+    size_t size;
+};
+typedef struct s_rectangle_array RectangleArray;
 
 struct s_shape_array {
     LineArray *lineArray;
+    TriangleArray *triangleArray;
+    RectangleArray *rectangleArray;
 };
 typedef struct s_shape_array ShapeArray;
 
@@ -20,6 +35,6 @@ typedef struct s_shape_array ShapeArray;
 ShapeArray *CreateEmptyShapeArray(void);
 
 int AddShapeToArray(ShapeArray *shapes, void *ptr, const unsigned int type);
-//int RemoveShapeFromArray(ShapeArray *shapes, void *shape, const unsigned int type);
+int RemoveShapeFromArray(ShapeArray *shapes, void *shape, const unsigned int type);
 
 #endif /* SHAPE_SHAPE_H_ */
