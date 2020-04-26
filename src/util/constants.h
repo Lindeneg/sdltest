a4682762ca50c8451eb9c6e6f82659f0825288df
Christian <christianlindeneg@gmail.com>
2020-04-26 12:03:38 +0200

    using enums for constants

--- src/util/constants.h
+++ src/util/constants.h
@@ -4,35 +4,53 @@
 #define EPSILON 0.00001
 #define PI 3.14159265359
 #define TWO_PI 2.0 * PI
-
 #define MAX_POLYGON_CORNERS 30
-
 #define TRIANGLE_SIZE 3
 #define RECTANGLE_SIZE 4
 #define CIRCLE_SIZE 6
 
-#define BLACK 0
-#define LIGHT_BLUE 1
-#define GRAY 2
-#define DARK_BLUE 3
-#define SILVER 4
-#define DARK_GREEN 5
-#define OLIVE 6
-#define TEAL 7
-#define BLUE 8
-#define GREEN 9
-#define PURPLE 10
-#define LIGHT_RED 11
-#define DARK_RED 12
-#define RED 13
-#define YELLOW 14
-#define WHITE 15
+typedef enum {
+    BLACK,
+    LIGHT_BLUE,
+    GRAY,
+    DARK_BLUE,
+    SILVER,
+    DARK_GREEN,
+    OLIVE,
+    TEAL,
+    BLUE,
+    GREEN,
+    PURPLE,
+    LIGHT_RED,
+    DARK_RED,
+    RED,
+    YELLOW,
+    WHITE
+} colors;
 
-#define VECTOR 0x7665
-#define LINE 0x6c69
-#define TRIANGLE 0x6e67
-#define RECTANGLE 0x726c
-#define CIRCLE 0x6363
+typedef enum {
+    VECTOR,
+    LINE,
+    TRIANGLE,
+    RECTANGLE,
+    CIRCLE
+} shapes;
+
+typedef enum {
+    PLAYER,
+    ENEMY,
+    NEUTRAL
+} collisionLayers;
+
+typedef enum {
+    NO_COLLISION,
+    PLAYER_PLAYER_COLLISION,
+    PLAYER_ENEMY_COLLISION,
+    ENEMY_PLAYER_COLLISION,
+    ENEMY_ENEMY_COLLISION,
+    PLAYER_NEUTRAL_COLLISION,
+    ENEMY_NEUTRAL_COLLISION,
+} collisionTypes;
 
 
 #endif /* UTIL_CONSTANTS_H_ */
